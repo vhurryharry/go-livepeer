c495d1b9871ae5dbcad5b5469975819057705bfe
Josh Allmann <joshua.allmann@gmail.com>
2020-07-01 19:16:23 +0000

    install_ffmpeg: Add bits to support pull and recording.
    * pipe protocol (pull)
    * stream_segment muxer (pull)
    * concat demuxer (recording)

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -115,8 +115,9 @@ if [ ! -e "$HOME/ffmpeg/libavcodec/libavcodec.a" ]; then
     --disable-encoders --disable-decoders --disable-filters --disable-bsfs \
     --disable-postproc --disable-lzma \
     --enable-gnutls --enable-libx264 --enable-gpl \
-    --enable-protocol=https,rtmp,file \
-    --enable-muxer=mpegts,hls,segment,mp4 --enable-demuxer=flv,mpegts,mp4,mov \
+    --enable-protocol=https,rtmp,file,pipe \
+    --enable-muxer=mpegts,hls,segment,stream_segment,mp4 \
+    --enable-demuxer=flv,mpegts,mp4,mov,concat \
     --enable-bsf=h264_mp4toannexb,aac_adtstoasc,h264_metadata,h264_redundant_pps \
     --enable-parser=aac,aac_latm,h264 \
     --enable-filter=abuffer,buffer,abuffersink,buffersink,afifo,fifo,aformat \
